7752b444cc040a176759713f0f197b645ceb7d28
ybbarng <ybbarng@gmail.com>
2014-11-11 23:02:30 +0900

    implement clipboard copy with ZeroClipboard

--- email-checker.js
+++ email-checker.js
@@ -18,5 +18,25 @@ $(function() {
     };
   }
 
+  function getEmails() {
+    var emails = '';
+    $('#email-list li.email').each(function() {
+      if($(this).find('input.valid').val()) {
+        emails += $(this).find('textarea.email').text();
+      }
+    });
+    return emails;
+  }
+
   $('#validate').click(splitEmail());
+
+  ZeroClipboard.config({
+    swfPath: 'ZeroClipboard.swf',
+    trustedDomains: ['*']
+  });
+  var clipboardManager = new ZeroClipboard($('#copy'));
+  clipboardManager.on('copy', function(event) {
+    var clipboard = event.clipboardData;
+    clipboard.setData('text/plain', getEmails());
+  });
 });
